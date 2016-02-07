8c1d07a45092fcd8663ea9d88df3aad71c8a80df
Erik Castricum <erikcas1972@gmail.com>
2016-02-07 23:48:38 +0100

    eagle: BoardConfig.mk is included by BoarConfigOmni.mk
    Change-Id: Ib219fb1e78b9e7d0cb6e22fbc10b6531f05023d7
    Signed-off-by: Erik Castricum <erikcas1972@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-include device/sony/yukon-common/BoardConfig.mk
 include device/sony/yukon-common/BoardConfigOmni.mk
 include device/sony/common/CommonConfigOmni.mk
 
