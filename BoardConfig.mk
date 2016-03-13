cdd1d93ddf1cf5a731794cddc60f5514adb69c9c
Erik Castricum <erikcas1972@gmail.com>
2016-03-13 02:24:33 +0100

    eagle: move and use init_sony
    requires https://gerrit.omnirom.org/#/c/17326/
    
    Change-Id: I8008db065bdc65c6904514676826965070d73c29
    Signed-off-by: Erik Castricum <erikcas1972@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,6 +14,8 @@
 
 include device/sony/yukon-common/PlatformConfigOmni.mk
 
+TARGET_SPECIFIC_HEADER_PATH += device/sony/eagle/include
+
 TARGET_RECOVERY_FSTAB = device/sony/eagle/rootdir/fstab.eagle
 
 TARGET_BOOTLOADER_BOARD_NAME := D2303
